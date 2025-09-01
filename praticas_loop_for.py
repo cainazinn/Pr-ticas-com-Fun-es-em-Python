8630961e86a087334e5556c610c451347d2cbad7
Cainã Cavalcante <cainacavalcante352@gmail.com>
2025-09-01 16:27:29 -0300

    Create praticas_loop_for.py

---
+++ praticas_loop_for.py
@@ -0,0 +1,40 @@
+
+# Desafio 1
+
+# n = int(input("Digite um número: "))
+# for i in range(1, 11):
+#     resultado = n * i
+#     print(n, "x", i, "=", resultado)
+
+# Desafio 2
+
+# import time
+
+# for i in range(10, -1, -1):
+#     print(i)
+#     time.sleep(1)
+
+# Desafio 3
+
+# for i in range(51):
+#     if i % 2 == 0:
+#         print(i)
+
+# Desafio 4
+
+# soma = 0
+
+# for i in range(1, 501, 2):
+#     if i % 3 == 0:
+#         soma += i
+# print(soma)
+
+# Desafio 5
+
+# soma = 0
+# for i in range(6):
+#     numero = int(input("Digite um número: "))
+#     if numero % 2 == 0:
+#         soma += numero
+# print(soma)
+
